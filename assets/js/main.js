ab7037cc058e7f2c2d16b5ebffffc59aed4136ea
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-12-07 15:56:11 +0700

    responsive trang home + section-8

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,15 @@ $('.slider-one').slick({
     slidesToShow: 6,
     // autoplay: true,
     // autoplaySpeed: 5000,
+    responsive: [
+        {
+          breakpoint: 576,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 2,
+          }
+        }
+       
+      ],
 });
 
