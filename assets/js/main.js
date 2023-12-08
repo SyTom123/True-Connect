422f8cbf63b6e263d3fcca04e78259776ed51da1
Nguyen Tien Sy <nguyentiensy.nts.9796@gmail.com>
2023-12-08 11:34:28 +0700

    section-14 trang about

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,4 +15,20 @@ $('.slider-one').slick({
        
       ],
 });
-
+$('.slider-two').slick({
+  infinite: true,
+  slidesToScroll: 2,
+  slidesToShow: 3,
+  // autoplay: true,
+  // autoplaySpeed: 5000,
+  responsive: [
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        }
+      }
+     
+    ],
+});
